41c068e740a1ea5dee25c441e9d3c217fc68a34b
Ryan Gunnar Schmidt <i1Schmidty@gmail.com>
2022-04-22 14:06:32 -0600

    asf

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-<script>
-
-        document.addEventListener('DOMContentLoaded', () => {
-        function createParagraph() {
-            const para = document.createElement('p');
-            para.textContent = 'You clicked the button!';
-            document.body.appendChild(para);
-        }
-
-        const buttons = document.querySelectorAll('button');
-
-        for (const button of buttons) {
-            button.addEventListener('click', createParagraph);
-        }
-        });
-      
-    </script>
+function createParagraph() {
+    const para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+  }
+  
+  const buttons = document.querySelectorAll('button');
+  
+  for (const button of buttons) {
+    button.addEventListener('click', createParagraph);
+  }
