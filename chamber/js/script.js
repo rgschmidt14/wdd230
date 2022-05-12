11ae42640dc80d6fa38d31cc1a22091e9bbefbc9
Ryan Gunnar Schmidt <i1Schmidty@gmail.com>
2022-05-12 10:16:37 -0600

    hgk

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -2,3 +2,47 @@ function toggleMenu() {
     document.getElementById("nav1").classList.toggle("clicked")
     document.getElementById("hamburgerBtn").classList.toggle("clicked")
 }
+
+monthsNames = [
+    "January",
+    "Febraury",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+    
+daysNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+function dTReader(date) { //give it a new Date() if you do not have a specific date in mind to give it.
+    const aMinute = 1000 * 60;
+    const anHour = aMinute * 60;
+    const aDay = anHour * 24;
+    const aYear = aDay * 365;
+    const d = date;
+    let years = Math.floor(d.getTime() / aYear) + 1970;
+    let months = monthsNames[Math.floor(((d.getTime() / aYear) - years + 1970) * 12)];
+    let dayOfWeek = daysNames[Math.floor(d.getDay())];
+    let day = d.getDate();
+    let minutes = `${d.getMinutes()<10?'0':''}${d.getMinutes()}`;
+    let timeOfDay = `${d.getHours()}:${minutes}`;
+    let dateCreated = [d,timeOfDay,dayOfWeek,months,day,years];
+    return dateCreated;
+}
+
+var readerOutcome1 = dTReader(new Date());
+document.getElementsByClassName("giveMeTodaysDate").innerHTML = ` ${readerOutcome[2]}, ${readerOutcome[4]} ${readerOutcome[3]} ${readerOutcome[5]}`;
