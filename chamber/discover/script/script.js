2bdbebcb6af32bdce97c96936cbb2782648d52e2
Ryan Gunnar Schmidt <i1Schmidty@gmail.com>
2022-06-07 15:57:04 -0600

    drop shadow

--- chamber/discover/script/script.js
+++ chamber/discover/script/script.js
@@ -47,8 +47,7 @@ function dTReader(date) { //give it a new Date() if you do not have a specific d
     let minutes = `${d.getMinutes()<10?'0':''}${d.getMinutes()}`;
     let timeOfDay = `${d.getHours()}:${minutes}`;
     let dateCreated = [d,timeOfDay,dayOfWeek,months,day,years]; //These are the outcomes: [0] is the date object, [1] the time, [2] the english word for the day of the week, [3] the english name for the month of the year, [4] the day of the month in number format, [5] the year.
-    return dateCreated;
-    
+    return dateCreated;  
 }
 
 var readerOutcome1 = dTReader(new Date());
@@ -82,3 +81,31 @@ if('IntersectionObserver' in window) {
         imageReplacer(img);
     });
 };
+
+//calculating days since last visit:
+if (!(localStorage.last_visit)) {
+    localStorage.last_visit = (new Date()).getTime();
+    console.log(localStorage.last_visit, "it worked")
+}
+
+
+function dateSubtractor(newDate, oldDate) {
+    console.log(newDate, oldDate)
+    let miliseconds = (newDate - oldDate);
+    let seconds = (miliseconds / 1000);
+    let minutes = (seconds / 60);
+    let hours = (minutes / 60);
+    let days = (hours / 24);
+    let returnObject = {
+        "days":days,
+        "hours":hours,
+        "minutes":minutes,
+        "seconds":seconds,
+        "default":miliseconds
+    }
+    console.log(returnObject)
+    return returnObject
+}
+
+let daysSinceLastVisit = dateSubtractor(readerOutcome1[0].getTime(), Number(localStorage.last_visit));
+document.querySelector("#visit-interval-calculated").innerHTML =  (Math.round(daysSinceLastVisit["days"] * 100) / 100);
