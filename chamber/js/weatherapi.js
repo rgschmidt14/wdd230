f0748a42507b49aef02d19f761a87774d929411e
Ryan Schmidt <blueboard2014@aol.com>
2022-06-26 01:23:39 -0600

    asdf

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -4,6 +4,7 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('.descOfWeather');
 const currentWindSpeed = document.querySelector('.windMph');
 
+
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=Ogden&units=imperial&APPID=15d92f0c10640be1ab22b43ff41477ab';
 //Ogden,ut,us
 
